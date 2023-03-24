9fdd50431b650b4349c86ccf795d835341cf1da4
Ênio F22 <79934130+assistente22@users.noreply.github.com>
2023-03-24 10:58:57 -0300

    Update segunda.py

--- segunda.py
+++ segunda.py
@@ -1,4 +1,4 @@
-numero = 10946 #MUDE AQUI O NUMERO A SER PROCURADO
+numero = 0 #MUDE AQUI O NUMERO A SER PROCURADO
 fib = 0
 fib_atual = 1
 
@@ -8,4 +8,4 @@ while(numero > fib_atual):
     fib_atual = fib+fib_atual
     fib = holder
 
-print(str(numero) + " pertence a sequência") if numero == fib_atual else print(str(numero) + " não pertence a sequência")
+print(str(numero) + " pertence a sequência") if (numero == fib_atual or numero == 0) else print(str(numero) + " não pertence a sequência")
